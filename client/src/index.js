6dee2ffe542c1f9239512685ae5e9b9faa815a48
Mirko Nasato <mirko@nasato.dev>
2022-04-04 18:39:51 +0100

    Create initial server and client projects

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import 'bulma/css/bulma.css';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
